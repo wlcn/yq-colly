2bc0bb1163d6274249935e4bc74c1992b9c0aa03
long.wang <wl20072757@163.com>
2019-03-21 19:36:45 +0800

    update

--- taihe/taihe.go
+++ taihe/taihe.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/url"
 	"os"
@@ -11,17 +12,35 @@ import (
 	"strings"
 	"time"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
 )
 
 const urlTemplate = "http://music.taihe.com/data/user/getsongs?start=%d&size=%d&ting_uid=%s&.r=%.10f"
 const lrcURLTemplate = "http://music.taihe.com/data/song/lrc?lrc_link=%s"
+const baseURL = "http://music.taihe.com%v"
 
 var regArtist, _ = regexp.Compile(`^/artist/[\d]+$`)
 var regSong, _ = regexp.Compile(`^/song/[\d]+$`)
 
 func init() {
 	rand.Seed(int64(time.Now().Nanosecond()))
+}
+
+// PageData struct
+type PageData struct {
+	Query struct {
+		Start   int     `json:"start"`
+		Size    int     `json:"size"`
+		TingUID int     `json:"ting_uid"`
+		R       float64 `json:"_r"`
+	} `json:"query"`
+	ErrorCode string `json:"errorCode"`
+	Data      struct {
+		HTML string `json:"html"`
+		Js   string `json:"js"`
+		CSS  string `json:"css"`
+	} `json:"data"`
 }
 
 func main() {
@@ -31,6 +50,7 @@ func main() {
 		colly.AllowedDomains("music.taihe.com"),
 	)
 	artistCollector := c.Clone()
+	artistPageCollector := c.Clone()
 	songCollector := c.Clone()
 	items := make([]string, 0, 10)
 	// Limit the number of threads started by colly to two
@@ -56,7 +76,7 @@ func main() {
 		if regSong.MatchString(link) {
 			fmt.Printf("Song found: %q -> %s\n", strings.TrimSpace(strings.Replace(e.Text, "\n", "", -1)), link)
 			// 歌曲列表
-			// songCollector.Visit(e.Request.AbsoluteURL(link))
+			songCollector.Visit(e.Request.AbsoluteURL(link))
 		}
 	})
 
@@ -75,7 +95,7 @@ func main() {
 		tingID := p[strings.LastIndex(p, "/")+1:]
 		u, _ := url.Parse(next)
 		m, _ := url.ParseQuery(u.RawQuery)
-		fmt.Printf("tingid is%v, start is %v, size is %v, songTotal is %v,", tingID, m["start"][0], m["size"][0], songTotal)
+		fmt.Printf("tingid is %v, start is %v, size is %v, songTotal is %v,", tingID, m["start"][0], m["size"][0], songTotal)
 
 		sizeInt, _ := strconv.Atoi(m["size"][0])
 		currInt, _ := strconv.Atoi(current)
@@ -84,15 +104,48 @@ func main() {
 			startInt := i * sizeInt
 			link := fmt.Sprintf(urlTemplate, startInt, sizeInt, tingID, rand.Float64())
 			fmt.Println("Page song found ", link)
-			artistCollector.Visit(e.Request.AbsoluteURL(link))
+			artistPageCollector.Visit(e.Request.AbsoluteURL(link))
 		}
 	})
 
+	// Verify response content
+	artistPageCollector.OnResponse(func(r *colly.Response) {
+		// fmt.Println("response", string(r.Body))
+		var pageData PageData
+		json.Unmarshal(r.Body, &pageData)
+		// fmt.Println(pageData.Data.HTML)
+		// Load the HTML document
+		doc, err := goquery.NewDocumentFromReader(strings.NewReader(pageData.Data.HTML))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		// fmt.Println(doc.Html())
+		// Find the review items
+		doc.Find("a").Each(func(_ int, s *goquery.Selection) {
+			// For each item found, get the band and title
+			link, _ := s.Attr("href")
+			if regSong.MatchString(link) {
+				title, _ := s.Attr("title")
+				fmt.Printf("Song found: %s -> %s\n", link, title)
+				songCollector.Visit(fmt.Sprintf(baseURL, link))
+			}
+		})
+	})
+
+	// On every ul element which has top_subnav__link class call callback
+	songCollector.OnHTML("div[id=lyricCont]", func(e *colly.HTMLElement) {
+		lrclink := e.Attr("data-lrclink")
+		fmt.Printf("Song lrclink found: %v\n", lrclink)
+	})
 	// On every ul element which has top_subnav__link class call callback
 	songCollector.OnHTML("div.song-info-box", func(e *colly.HTMLElement) {
 		songName := e.ChildText("span.name")
 		artist := e.ChildText("span.artist a")
-		fmt.Printf("Song detail found: %v, %q\n", songName, artist)
+		album := e.ChildText("p.album a")
+		publish := e.ChildText("p.publish")
+		company := e.ChildText("p.company")
+		fmt.Printf("Song detail found: %v, %v, %v, %v, %v\n", songName, artist, album, publish, company)
 	})
 
 	// Before making a request print "Visiting ..."
