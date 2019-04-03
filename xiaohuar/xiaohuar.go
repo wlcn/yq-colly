9ecc901350fe8da13f3b07a1b409b294d5f97cc6
long.wang <wl20072757@163.com>
2019-04-03 19:39:38 +0800

    add xiaohuar

---
+++ xiaohuar/xiaohuar.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gocolly/colly"
+	"github.com/wlcn/yq-colly/common"
+	"github.com/wlcn/yq-colly/producer"
+	"github.com/wlcn/yq-starter/service/article"
+)
+
+func main() {
+	// Instantiate default collector
+	c := colly.NewCollector()
+
+	p := producer.NewSyncProducer()
+	defer producer.CloseSync(p)
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.OnHTML(`html`, func(e *colly.HTMLElement) {
+		url := e.Request.URL.String()
+		e.ForEach(`a`, func(_ int, a *colly.HTMLElement) {
+			itemURL := e.Request.AbsoluteURL(e.Attr("href"))
+			if strings.Index(itemURL, "www.xiaohuar.com") != -1 {
+				c.Visit(itemURL)
+			}
+		})
+		if strings.Index(url, "http://www.xiaohuar.com/list-") != -1 {
+			e.ForEach("div.img a", func(_ int, a *colly.HTMLElement) {
+				data := article.Article{
+					Title:   a.Attr("alt"),
+					PicLink: a.Attr("href"),
+					Content: a.Text,
+				}
+				producer.SendSync(p, common.Topic, data)
+			})
+		}
+	})
+
+	c.Visit("http://www.xiaohuar.com/hua/")
+
+}
