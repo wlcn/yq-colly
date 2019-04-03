48a8987b403291bb23a22c1ab5b384285ac3532c
wlcn <362747160@qq.com>
2019-04-03 22:03:39 +0800

    update

--- xiaohuar/xiaohuar.go
+++ xiaohuar/xiaohuar.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/gocolly/colly"
 	"github.com/wlcn/yq-colly/common"
 	"github.com/wlcn/yq-colly/producer"
-	"github.com/wlcn/yq-starter/service/image"
 )
 
 func main() {
@@ -22,24 +20,21 @@ func main() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	c.OnHTML(`html`, func(e *colly.HTMLElement) {
-		url := e.Request.URL.String()
-		e.ForEach(`a`, func(_ int, a *colly.HTMLElement) {
-			itemURL := e.Request.AbsoluteURL(e.Attr("href"))
-			if strings.Index(itemURL, "www.xiaohuar.com") != -1 {
-				c.Visit(itemURL)
+	c.OnHTML(`div.demo`, func(e *colly.HTMLElement) {
+		e.ForEach(`div.item_t`, func(_ int, d *colly.HTMLElement) {
+			data := map[string]string{
+				"Title":   d.ChildText("span.price"),
+				"URL":     e.Request.AbsoluteURL(d.ChildAttr("img", "src")),
+				"Content": d.ChildAttr("img", "alt"),
+			}
+			producer.SendSync(p, common.Topic, data)
+		})
+		e.ForEach(`div.page a`, func(_ int, a *colly.HTMLElement) {
+			if a.Text == "下一页" {
+				nextLink := a.Attr("href")
+				c.Visit(e.Request.AbsoluteURL(nextLink))
 			}
 		})
-		if strings.Index(url, "http://www.xiaohuar.com/list-") != -1 {
-			e.ForEach("div.img a", func(_ int, a *colly.HTMLElement) {
-				data := image.Image{
-					Title:   a.Attr("alt"),
-					URL:     a.Attr("href"),
-					Content: a.Text,
-				}
-				producer.SendSync(p, common.Topic, data)
-			})
-		}
 	})
 
 	c.Visit("http://www.xiaohuar.com/hua/")
